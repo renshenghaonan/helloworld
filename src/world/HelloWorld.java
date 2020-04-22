4b482af7f633b805eda542f4f0009400f75524f7
renshenghaonan <1103172598@qq.com>
2020-04-22 08:36:39 +0800

    test

--- src/world/HelloWorld.java
+++ src/world/HelloWorld.java
@@ -2,6 +2,6 @@ package world;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("HHHHelloWorld你好啊啊啊啊啊啊啊啊啊啊");
+		System.out.println("1HHHHelloWorld你好啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊");
 	}
 }
